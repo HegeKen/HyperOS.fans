d730db9ed559e5e2e05b964406942ca9d0425158
Hege <hegeken@foxmail.com>
2023-10-21 20:41:31 +0800

    disclaimer

--- pages/index.vue
+++ pages/index.vue
@@ -9,5 +9,6 @@ useHead({
     <div class="Center" style="font-size: 50px;">Xiaomi Hyper<span class="HyperBlue">OS</span></div>
     <div class="Center" style="font-size: 40px;">小 米 澎 湃 O S</div>
     <div class="Center" style="font-size: 30px;font-weight: lighter;">Coming soon.....</div>
+    <div class="Center" style="font-weight: lighter;">...非官方网站...</div>
   </div>
 </template>
