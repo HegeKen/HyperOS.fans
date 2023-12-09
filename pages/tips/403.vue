4c4deb49ba30efb0880f7a481fe2b27ad174dbd3
Hege <hegeken@foxmail.com>
2023-12-09 13:46:47 +0800

    2023.12.09 13:46:31

--- pages/tips/403.vue
+++ pages/tips/403.vue
@@ -9,8 +9,8 @@
         <p>Windows : <code>C:\Windows\System32\drivers\etc\hosts</code><br />Linux/MacOS : <code>/etc/hosts</code></p>
         <li>
           {{ $t('1step2') }}
-          <p><code>123.6.13.6 bigota.d.miui.com<br />
-                   123.6.13.6 hugeota.d.miui.com</code></p>
+          <p><code>183.136.207.132 bigota.d.miui.com<br />
+                   183.136.207.132 hugeota.d.miui.com</code></p>
         </li>
         <li>{{ $t('1step3') }}</li>
       </ol>
@@ -21,7 +21,7 @@
       <h3>{{ $t('method2') }}</h3>
       <ol>
         <li>点击以下链接进行下载，感谢: <a href="https://www.coolapk.com/u/13562649" target="_blank">忆雨_Micles@酷安</a> 提供脚本</li>
-        <p><a href="/403FixBy忆雨_V1.0.5.3.bat" download="/403FixBy忆雨_V1.0.5.3.bat">403FixBy忆雨_V1.0.5.3.bat</a></p>
+        <p><a href="/403FixBy忆雨_V2.0.2.bat" download="/403FixBy忆雨_V2.0.2.bat">403FixBy忆雨_V2.0.2.bat</a></p>
         <li>下载完成后，双击运行该脚本，并按步骤进行操作。</li>
       </ol>
     </div>
