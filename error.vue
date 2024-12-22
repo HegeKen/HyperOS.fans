db650bccaad556aeccce0657402f691d2607f116
HegeKen <hegeken@foxmail.com>
2024-12-22 23:43:04 +0800

    wts

---
+++ error.vue
@@ -0,0 +1,11 @@
+<template>{{ error.statusCode }}</template>
+<script setup>
+const error = useError()
+let url = useRequestURL()
+const route = useRoute()
+let domain = url.hostname
+if (domain == "https://www.hyperos.fans") {
+  url = 'https://hyperos.fans' + route.path
+  await navigateTo(url, { external: true })
+}
+</script>
