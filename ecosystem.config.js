81fce7dc2e532a5d56d20be1e2f0e1b244040830
HegeKen <hegeken@foxmail.com>
2024-12-23 14:21:26 +0800

    pm2 adjust

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   apps: [
     {
-      name: 'NuxtAppName',
+      name: 'HyperOS.fans',
       port: '80',
+      host: 'hyperos.fans',
       exec_mode: 'cluster',
       instances: 'max',
       script: './.output/server/index.mjs'
