e83fd0deba52b1b8d88284982085c55088305855
Hege <hegeken@foxmail.com>
2026-08-23 13:15:41 +0800

    feat(index, i18n: add hub banner promotion for new ROM lookup site
    - 新增了hub-banner 横幅组件，添加国际化文案，跳转至新的ROM固件查询站点
    - 更新了子模块提交记录

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -7,6 +7,18 @@
         <Nav></Nav>
       </ClientOnly>
       <main id="main-content" tabindex="-1">
+        <ClientOnly>
+          <a href="https://hub.miuier.com" target="_blank" rel="noopener noreferrer" class="hub-banner-link">
+            <v-alert type="info" variant="tonal" rounded="lg" class="hub-banner" border="start" border-color="amber-darken-2">
+              <div class="d-flex align-center flex-wrap">
+                <v-icon icon="mdi-open-in-new" size="20" class="mr-2" color="amber-darken-2"></v-icon>
+                <span class="text-subtitle-1 font-weight-bold mr-2 hub-banner-highlight">MiROMs HUB</span>
+                <span class="mr-2 hub-banner-sep">|</span>
+                <span>{{ $t('hubBanner') }}</span>
+              </div>
+            </v-alert>
+          </a>
+        </ClientOnly>
         <v-card elevation="2">
           <v-card-item>
             <v-card-title class="text-HyperBlue">
